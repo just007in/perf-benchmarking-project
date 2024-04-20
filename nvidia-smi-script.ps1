436288a21828822544fd1ddd9afb12d13eb96e08
Justin Lee <just007in2000@gmail.com>
2024-04-19 19:53:36 -0500

    changed to csv

--- nvidia-smi-script.ps1
+++ nvidia-smi-script.ps1
@@ -2,6 +2,5 @@ $Game = Read-Host "Please enter game name (FH4, TR, CP, R6S)"
 $Preset = Read-Host "Please enter preset name (low, medium, ultra)"
 $Tmp = Read-Host "Get a stopwatch ready. Hit enter and start the timer. Stop the timer when you see the first scene of the benchmark."
 Write-Output "Starting Measurement"
-nvidia-smi --id=0 -q -x --loop=1 --filename="$Game"_"$Preset".xml
-
+nvidia-smi --query-gpu=timestamp,temperature.gpu,temperature.memory,utilization.gpu,utilization.memory,utilization.encoder,utilization.decoder,utilization.jpeg,utilization.ofa,clocks.gr,clocks.sm,clocks.mem,clocks.video,power.draw,pstate --format=csv --loop=1 --filename="$Game"_"$Preset".csv
 
