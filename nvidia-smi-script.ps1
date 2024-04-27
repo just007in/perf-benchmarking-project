9fb46066acd8b09ee10e831c5bbd6c68674f9be4
Justin Lee <just007in2000@gmail.com>
2024-04-27 14:47:00 -0500

    added memory used

--- nvidia-smi-script.ps1
+++ nvidia-smi-script.ps1
@@ -2,4 +2,4 @@ $Game = Read-Host "Please enter game name (FH4, TR, CP, R6S)"
 $Preset = Read-Host "Please enter preset name (low, medium, ultra)"
 $Tmp = Read-Host "Get a stopwatch ready. Hit enter and start the timer. Stop the timer when you see the first scene of the benchmark."
 Write-Output "Starting Measurement"
-nvidia-smi --query-gpu=timestamp,temperature.gpu,utilization.gpu,utilization.memory,clocks.gr,clocks.mem,clocks.video,power.draw,pstate --format=csv --loop=1 --filename="$Game"_"$Preset".csv
+nvidia-smi --query-gpu=timestamp,temperature.gpu,utilization.gpu,utilization.memory,clocks.gr,clocks.mem,clocks.video,power.draw,pstate,memory.used --format=csv --loop=1 --filename="$Game"_"$Preset".csv
